ac4de4ff656e09c589b7376f44b202b728b8586f
thomasabc1999 <cph-th349@cphbusiness.dk>
2025-01-27 11:38:59 +0000

    opdateret SQL script

--- MySQLScript.sql
+++ MySQLScript.sql
@@ -1,9 +1,38 @@
 
-select distinct scrapetime from anholt;
+CREATE TABLE `anholt` (
+  `starttid` datetime DEFAULT NULL,
+  `NO2` decimal(10,2) DEFAULT NULL,
+  `NOX` decimal(10,2) DEFAULT NULL,
+  `ScrapeTime` datetime DEFAULT NULL
+);
+CREATE TABLE `banegaard` (
+  `starttid` datetime DEFAULT NULL,
+  `CO` decimal(10,2) DEFAULT NULL,
+  `NO2` decimal(10,2) DEFAULT NULL,
+  `NOX` decimal(10,2) DEFAULT NULL,
+  `ScrapeTime` datetime DEFAULT NULL
+);
+CREATE TABLE `hcandersen` (
+  `starttid` datetime DEFAULT NULL,
+  `CO` decimal(10,2) DEFAULT NULL,
+  `NO2` decimal(10,2) DEFAULT NULL,
+  `NOX` decimal(10,2) DEFAULT NULL,
+  `SO2` decimal(10,2) DEFAULT NULL,
+  `O3` decimal(10,2) DEFAULT NULL,
+  `PM10` decimal(10,2) DEFAULT NULL,
+  `PM2.5` decimal(10,2) DEFAULT NULL,
+  `ScrapeTime` datetime DEFAULT NULL
+);
+CREATE TABLE `risoe` (
+  `starttid` datetime DEFAULT NULL,
+  `CO` decimal(10,2) DEFAULT NULL,
+  `NO2` decimal(10,2) DEFAULT NULL,
+  `NOX` decimal(10,2) DEFAULT NULL,
+  `O3` decimal(10,2) DEFAULT NULL,
+  `PM10` decimal(10,2) DEFAULT NULL,
+  `ScrapeTime` datetime DEFAULT NULL
+);
 
-select * from anholt 
-where scrapetime = '2025-01-26 17:00:49';
 
-select count(*) from banegaard;
 
-SELECT * FROM miljødata.anholt where NOX >3;
+
